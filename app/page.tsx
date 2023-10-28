4ddedd590ae9603723a3895419e29c5548dbccc3
Sahachindech Katedee <86822195+Pikachu051@users.noreply.github.com>
2023-10-29 00:27:49 +0700

    Update Poster section

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import {
 import { buttonVariants } from '@/components/ui/button';
 import Image from "next/image";
 import Link from "next/link";
+import { ArrowDownToLine, ScrollText } from 'lucide-react';
 
 export default function Home() {
   return <PageWrapper>
@@ -18,14 +19,25 @@ export default function Home() {
         <div className='w-full p-5 px-24 flex justify-center'>
           <div className='p-5 bg-stone-200 drop-shadow-xl backdrop-blur-sm rounded-md grid grid-cols-2 grid-flow-col gap-10'>
             <div className=''>
-              <p className='text-lg'><strong>CWIZ (ออกเสียงว่า ควิซ)</strong> เป็นเกมตอบคำถามทั่วไปที่ถูกสร้างขึ้นด้วยภาษา C ซึ่งลักษณะของคำถามจะเป็นคำถามสำหรับการทดสอบความรู้รอบตัว</p>
+              <p className='text-lg'><strong>CWIZ (ออกเสียงว่า ควิซ)</strong> เป็นเกมตอบคำถามทั่วไปที่ถูกสร้างขึ้นด้วยภาษา C ซึ่งลักษณะของคำถามจะเป็นคำถามสำหรับการทดสอบความรู้รอบตัว โดยผู้เล่นต้องเลือกคำตอบที่ถูกต้องที่สุด ยิ่งตอบเร็ว ยิ่งได้คะแนนเยอะ! แต่ถ้าตอบผิด ผู้เล่นก็จะถูกลบคะแนนเป็นการลงโทษนั่นเอง</p>
             </div>
             <div className='flex justify-center'>
               <iframe className="rounded-xl" width="600" height="300" src="https://www.youtube.com/embed/ZPYwM3NOSMg" title="LANY - It Even Rains in LA (Live Performance) | Vevo" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
           </div>
         </div>
-        <div className='w-full px-24 pt-10 pb-10'><div className='bg-stone-200 rounded-md p-5'>Poster<div className='align-top flex justify-end'><Link className={buttonVariants({variant: "outline"})} href="https://www.pinterest.com" passHref={true} >Download</Link></div></div></div>
+        <div className='w-full px-24 pt-10 pb-10'>
+          <div className='bg-stone-200 rounded-md pt-5 px-5 pb-[-12]'>
+            <strong className='font-bold text-xl'>Poster</strong>
+            <div className='flex justify-start pl-2 pt-2'><div className='px-2 scale-75'><ScrollText /></div> โปสเตอร์ Project CWIZ ขนาด A1</div>
+            <div className='flex justify-end relative bottom-8'>
+              <Link className={buttonVariants({variant: "outline"})} href="https://www.pinterest.com" passHref={true}>
+                <div className='scale-75'><ArrowDownToLine/></div>
+                <div className='pl-2'>Download</div>
+              </Link>
+            </div>
+          </div>
+        </div>
         <div className='w-full px-24 pt-8'><h1 className='text-white text-4xl font-bold'>FAQ</h1></div>
         <div className='pt-8 px-24 pb-24 w-full flex justify-center text-white'>
           <Accordion type="single" collapsible className="w-full">
